3c362eb02cd99f3ee7dfd653fb8a9d1de70e609b
Khanh091 <nvkhanh0911@gmail.com>
2025-09-22 21:53:10 +0700

    add chat schema, resolvers, and subscriptions

--- pubsub.js
+++ pubsub.js
@@ -1,16 +1,6 @@
-import { RedisPubSub } from "graphql-redis-subscriptions";
 import { PubSub } from "graphql-subscriptions";
-import Redis from "ioredis";
 
-const options = {
-  host: "127.0.0.1",
-  port: 6379,
-  retryStrategy: (times) => Math.min(times * 50, 2000),
-};
-export const pubsub = new RedisPubSub({
-  publisher: new Redis(options),
-  subscriber: new Redis(options),
-});
+export const pubsub = new PubSub();
 export const EVENTS = {
   MODEL_CREATED: "MODEL_CREATED",
   FRIEND_REQUESTED: "FRIEND_REQUESTED",
